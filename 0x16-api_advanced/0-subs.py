4b5e849a2dc5c1911861eb3bd4704e67aac0444e
SPaci20 <spaci20@gmail.com>
2024-01-18 07:04:43 -0800

    minor update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    # Check for successful response (status code 200)
-    if response.status_code == 200:
-        try:
-            results = response.json().get("data")
-            return results.get("subscribers")
-        except ValueError as e:
-            print(f"Error parsing JSON: {e}")
-            return 0
-    elif response.status_code == 404:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    else:
-        print(f"Error: {response.status_code}")
-        print(response.text)  # Print the response content for debugging
+
+    user_agent = {'User-agent': 'Google Chrome Version 120.0.6099.216'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
