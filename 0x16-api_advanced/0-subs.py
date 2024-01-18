a2232f7f7700f1f0ae3ad2ab504936d145fad2aa
SPaci20 <spaci20@gmail.com>
2024-01-18 07:12:06 -0800

    minor updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,9 +13,9 @@ def number_of_subscribers(subreddit):
     """
 
     if subreddit is None or not isinstance(subreddit, str):
-        return 0
+        return 0`
 
-    user_agent = {'User-agent': 'Google Chrome Version 120.0.6099.216'}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
